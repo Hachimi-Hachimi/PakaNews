6e157ffa2f6489d16787af7bf656fb42a500413c
LeadRDRK <leadrdrk@zohomail.com>
2024-06-24 19:10:01 +0700

    Minor changes

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 function formatTimestamp(timestamp: number) {
-    return new Date(timestamp * 1000).toLocaleString(undefined, {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'});
+    return new Date(timestamp * 1000).toLocaleString("en-US", {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'});
 }
 
 export default {
